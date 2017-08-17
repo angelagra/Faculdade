25631d56f4f7dd8bbb630ac1353f92de62ad3fc6
Angela Graciete <angelagraciete84@gmail.com>
2017-08-16 22:18:36 -0300

    Add files via upload

---
+++ Sistemas_Para_Internet/3º Semestre/Programação Orientada a Objeto/Aula 2/Exercicio 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+
+            Console.WriteLine("Qauntos Numeros?: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Corte: ");
+            int corte = int.Parse(Console.ReadLine());
+
+            int[] vetor = new int[n];
+
+            for(int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = rng.Next(100);
+            }
+
+            for(int i = 0; i < vetor.Length; i++)
+            {
+                if(vetor[i] > corte)
+                {
+                    Console.WriteLine(vetor[i); 
+                }
+            }
+        }
+    }
+}
---
+++ Sistemas_Para_Internet/3º Semestre/Programação Orientada a Objeto/Aula 2/Exercicio1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ENTRADAS
+            Console.Write("Escreva seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Escreva o Valor da hora de trabalho: ");
+            decimal valor = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Horas Trabalhadas: ");
+            double hora = double.Parse(Console.ReadLine());
+
+            //PROCESSAMENTO
+            decimal salario = valor * Convert.ToDecimal(hora);
+            string msg = string.Format("O Funcionario {0} trabalhou {1} horas por R$ {2:F2} e deve receber R$ {3:F2}", nome, hora, valor, salario);
+
+
+            //SAIDA
+            // Console.WriteLine("O Funcionário " + nome + " Trabalhou " + hora + "horas por R$ " + valor + " e deve receber R$ " + salario ); da para fazer assim tambem
+            //Console.WriteLine("O Funcionario {0} trabalhou {1} horas por R$ {2:F2} e deve receber R$ {3:F2}", nome, hora, valor, salario ); //F significa que será um decimal e 2 será com duas casas apos a virgula
+            Console.WriteLine(msg);
+        }
+    }
+}
