81345eb18cdf7348b4d34b4dfca114aad0cd0b7c
Angela Graciete <angelagraciete84@gmail.com>
2017-08-23 22:36:26 -0300

    Add files via upload

---
+++ Sistemas_Para_Internet/3º Semestre/Programação Orientada a Objeto/Aula 3/ConverterTemperatura/temperatura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterTemperatura
+{
+    class Temperatura
+    {
+        public double f;
+
+        public Temperatura(double f)
+        {
+            this.f = f;
+        }
+
+        public double calculo()
+        {
+            return (5.0/9.0 * (f - 32));
+        }
+    }
+}
---
+++ Sistemas_Para_Internet/3º Semestre/Programação Orientada a Objeto/Aula 3/MediaAluno/MediaAluno/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ENTRADA
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("P1: ");
+            double p1 = double.Parse(Console.ReadLine());
+
+            Console.Write("P2: ");
+            double p2 = double.Parse(Console.ReadLine());
+
+            //PROCESSAMENTO
+            Aluno a = new Aluno(nome, p1, p2);
+
+            double media = a.CalculaMedia();
+            bool aprovado = a.EstaAprovado();
+
+            if (a.EstaAprovado())
+            {
+                Console.WriteLine("O Aluno {0} foi aprovado com média {1}", a.nome, media);
+            }else
+            {
+                Console.WriteLine("O aluno {0} foi reprovado com média {1}", a.nome, media);
+            }
+        }
+    }
+}
