1002980bc36b884de1682e3e2de316cd3951e6ce
Angela Graciete <angelagraciete84@gmail.com>
2017-09-13 22:31:04 -0300

    Add files via upload

---
+++ Sistemas_Para_Internet/3º Semestre/Programação Orientada a Objeto/Aula 6/ResoluçãoExercicio_Funcionario/ResoluçãoExercicio_Funcionario/Funcionario.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResoluçãoExercicio_Funcionario
+{
+    class Funcionario
+    {
+
+        private string nome;
+        private int nivel;
+        private double horas;
+
+        public Funcionario(string nome, int nivel, double horas)
+        {
+            this.nome = nome;
+            this.nivel = nivel;
+            this.horas = horas;
+            
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        public double GetHoras()
+        {
+            return horas;
+        }
+
+        public decimal GetValor()
+        {
+            decimal valor = 0;
+
+            switch (nivel)
+            {
+                case 1: valor = 15; break;
+                case 2: valor = 18; break;
+                case 3: valor = 22; break;
+                case 4: valor = 27; break;
+                case 5: valor = 33; break;
+            }
+
+            return valor;
+        }
+
+        public int GetNivel()
+        {
+            return nivel;
+        }
+
+
+        public decimal GetSalario()
+        {
+            decimal valor = GetValor();
+
+            return Convert.ToDecimal(horas) * valor;
+        }
+
+        public void Promover()
+        {
+            if(nivel < 5) { 
+                nivel = nivel + 1;
+            }
+        }
+
+    }
+}
---
+++ Sistemas_Para_Internet/3º Semestre/Programação Orientada a Objeto/Aula 6/Sobrecarga/Sobrecarga/Aluno.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrecarga
+{
+    class Aluno
+    {
+
+        private string nome;
+        private double p1, p2;
+
+        //CONSTRUTORES
+        public Aluno(string nome, double p1, double p2)
+        {
+            Console.WriteLine("Executa em primeiro");
+            this.nome = nome;
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+
+        public Aluno(string nome) : this(nome, 10, 10) // chama o outro construtor 
+        {
+            Console.WriteLine("Executa em segundo");
+        }
+
+        //METODOS
+        public double GetMedia()
+        {
+            return (p1 + p2) / 2;
+        }
+
+        public bool GetAprovado()
+        {
+            double media = GetMedia();
+
+            return media >= 6;
+        }
+
+        public bool GetAprovado(double media)
+        {
+            return media >= 6; 
+        }
+       
+    }
+}
