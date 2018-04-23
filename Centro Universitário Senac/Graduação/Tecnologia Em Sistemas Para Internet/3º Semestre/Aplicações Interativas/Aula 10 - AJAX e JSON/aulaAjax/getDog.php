31c3b55afd35bbc31472c554c574ae24c78e0a53
Angela Graciete <angelagraciete84@gmail.com>
2018-04-23 14:34:52 -0300

    jquery

---
+++ Centro Universitário Senac/Graduação/Tecnologia Em Sistemas Para Internet/3º Semestre/Aplicações Interativas/Aula 10 - AJAX e JSON/aulaAjax/getDog.php
@@ -0,0 +1,34 @@
+<?php
+
+	$idDog = $_GET["idDog"];
+	if ($idDog=="labrador") {
+
+?>
+
+	<div>
+		<img style="width: 150px;" src="YellowLabradorLooking_new.jpg"></img>
+		<p>Os primeiros Labradores eram cães d'água que surgiram à partir dos antigos cães Terra-nova, e receberam este nome por serem nativos da região chamada Labrador. este canino teria chegado a Newfoundland, no Canadá, levado ou pelos portugueses ou pelos bascos, ou ainda pelos exploradores escandinavos. O cão Terra-nova não apenas deu origem ao Labrador como também inicialmente era chamado de Labrador. Os Terra-nova do começo de 1800 tinham diferentes tamanhos, sendo o menor deles, “Lezer” ou “Cão de Saint John”, a primeira encarnação do Labrador moderno. Esses últimos eram cães pretos, de tamanho médio e pêlo curto, e não apenas recolhiam caças abatidas, mas também peixes, puxando pequenos barcos de pesca nas águas geladas e ajudando os pescadores em todas as tarefas em que precisasse nadar.</p>
+	</div>
+							
+<?php  
+							
+	}  else if ($idDog == "pastorAlemao") {
+		
+?>
+
+	<img style="width: 150px;" src="DSHwiki.jpg"></img>
+	<p>Na Alemanha durante o século XIX existiam três variedades de cães de pastoreio na região da Suábia, Turíngia e Württemberg, respectivamente.[3][4][5] Segundo relatos, estes cães seriam híbridos de lobos com cães trazidos à estas regiões. Especula-se ainda um possível parentesco com a raça Pastor-da-boêmia, que é bastante antiga, mas não há fatos que comprovem esta relação.[6]Ao final do século XIX teve início, ainda que sutil, um movimento pela padronização dos cães alemães com a fundação da Phylax Society em 1891, que reuniu também as variedades de cães pastores com este mesmo fim. Porém a associação não teve sucesso, sendo dissolvida apenas três anos depois devido à discordância entre os criadores, já que alguns acreditavam que os cães deveriam ser selecionados pela funcionalidade, e outros acreditavam que deveria ser pela morfologia.[7][8]Um ex-membro da Phylax Society, o ex-capitão da cavalaria alemã chamado Max von Stephanitz, também ex-estudante da Escola de Veterinária de Berlim, adquiriu um cão pastor da Turíngia chamado Hektor Linksrhein, que lhe cativou pelas ótimas habilidades para trabalho.</p> 
+							
+<?php  
+							
+	}  else  {
+		
+?>							
+			
+	<img style="width: 150px;" src="Shih-Tzu.jpg"></img>
+	<p>Bem como outras raças caninas antigas, a origem deste animal possui uma lenda. A lenda define o shih tzu como sendo o símbolo do amor impossível entre uma princesa chinesa e um mongol(povo predominante no Tibete). Segundo essa lenda, diante da impossibilidade de realizarem o casamento, o casal resolveu cruzar um legítimo representante da China, o Pequinês, com um representante do Tibete, o Lhasa Apso. Da união das raças surgiu o Shih-Tzu, simbolizando tudo o que há melhor nas duas culturas, e o amor entre os dois povos.[2] Seu nome significa "cão leão que nunca desiste". Sua verdadeira origem perdeu-se em meio a antigas lendas, embora saiba-se que alguns exemplares foram dados de presente ao imperador da China em 1640. De acordo com historiadores, os shi-tzus viviam no palácio, cercados de cuidados extremos e isolados de outras raças. Apenas três séculos mais tarde, começaram a fazer parte dos lares das ricas famílias chinesas e de algumas outras no Ocidente. Quase extinta durante a invasão japonesa em 1937, foi salva graças a criadores ingleses</p>
+
+<?php
+	}
+?>	
+
